42432d4868ec7a0ec4f65b7c90db8aab4f4cab92
Olle Ronstad <Olle.Ronstad@gmail.com>
2023-11-24 14:20:20 +0100

    made to_graph more general and added scaling

--- to_graph.py
+++ to_graph.py
@@ -9,41 +9,43 @@ lines = file.readlines()
 
 def to_mil(in_str):
     number_part = re.match("[\d\.]+",in_str)[0]
-    if re.search("µs",in_str):
+    if re.search("ns",in_str):
+        number_part = float(re.match("[\d\.]+",in_str)[0])/1_000_000
+    elif re.search("µs",in_str):
         number_part = float(re.match("[\d\.]+",in_str)[0])/1000
-        
     elif re.search("ms",in_str):
         number_part = float(re.match("[\d\.]+",in_str)[0])
     elif re.search("s",in_str):
         number_part = float(re.match("[\d\.]+",in_str)[0])*1000
     return number_part
-    
-n = []
-k_n = []
-k_n_div_2 = []
-k_n_div_4 = []
-k_n_div_16 = []
-k_n_div_32 = []
-k_const_12 = []
 
-lines = lines[2:]
+
+header = lines[0].split(", ")
+data = {}
+for i in header:
+    data[i] = []
+
+lines = lines[1:]
 for i in range(0,len(lines)):
     lines[i] = lines[i].split(", ")
-    n.append(int(lines[i][0]))
-    k_n.append(to_mil(lines[i][1]))
-    k_n_div_2.append(to_mil(lines[i][2]))
-    k_n_div_4.append(to_mil(lines[i][3]))
-    k_n_div_16.append(to_mil(lines[i][4]))
-    k_n_div_32.append(to_mil(lines[i][5]))
-    k_const_12.append(to_mil(lines[i][6]))
-plt.plot(n,k_n,label="k=n")
-plt.plot(n,k_n_div_2,label="k=n/2")
-plt.plot(n,k_n_div_4,label="k=n/4")
-plt.plot(n,k_n_div_16,label="k=n/16")
-plt.plot(n,k_n_div_32,label="k=n/32")
-plt.plot(n,k_const_12,label="k=12")
-plt.legend(["k=n","k=n/2","k=n/4","k=n/16","k=n/32","k=12"])
-plt.xlabel("n")
-plt.ylabel("Time ms")
+    data[header[0]].append(float(lines[i][0]))
+    for j in range(1, len(header)):
+        data[header[j]].append(to_mil(lines[i][j]))
+
+
+if "-s" in sys.argv:
+    path = path +".s"
+    for i in header[1:]:
+        # div = min(data[i])
+        div = data[i][0]
+        for j in range(0,len(data[i])):
+            data[i][j] = float(data[i][j])/div
+
+for i in header[1:]:
+    plt.plot(data[header[0]],data[i])
+
+plt.legend(header[1:])
+plt.xlabel(header[0])
+plt.ylabel("Time")
 plt.savefig(path+".png")
 
